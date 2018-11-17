398520f015c2e82e25fea315fbbc1cf0a14c34b6
Sean Lewis <slewis@novacoast.com>
2018-11-17 16:18:14 +0000

    Adds new markdown and styling

--- javascript/main.js
+++ javascript/main.js
@@ -3,10 +3,19 @@ function setOptions() {
   showdown.setFlavor('github');
 } // setOptions
 
+function renderIndex() {
+  loadMarkdownFromFile('markdown/index.md');
+}
+
 // Given a files location, load that file and update the html
 function loadMarkdownFromFile(file) {
   setOptions();
   var converter = new showdown.Converter();
+
+  // Check file has been specified
+  if(file === '') {
+    alert("No file specified");
+  }
 
   $.ajax({
     url: file,
