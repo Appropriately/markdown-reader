54a020dbb08216ec086b2503c6306484c58c2f2b
Sean Lewis <slewis@novacoast.com>
2018-11-18 17:36:29 +0000

    Allows page navigation using url params

--- javascript/main.js
+++ javascript/main.js
@@ -1,3 +1,18 @@
+// Function that will handle the site being loaded
+function onPageLoad() {
+  // Check for URL params
+  var url = new URL(window.location.href);
+  var page = url.searchParams.get("page");
+
+  // Check if there are any URL params
+  if (page === null || page.slice(-2) !== 'md') {
+    renderIndex();
+  } else {
+    loadMarkdownFromFile(page);
+  }
+
+} // onPageLoad
+
 // Set the options for the showdown markdown convertor
 function setOptions() {
   showdown.setFlavor('github');
@@ -6,6 +21,11 @@ function setOptions() {
 function renderIndex() {
   loadMarkdownFromFile('markdown/index.md');
 }
+
+function handleBadLink(link) {
+  alert("What the hell is " + link); // TODO: Make this give a page alert instead
+  renderIndex(); // Default to the index page.
+} // handleBadLink
 
 // Given a files location, load that file and update the html
 function loadMarkdownFromFile(file) {
@@ -23,7 +43,14 @@ function loadMarkdownFromFile(file) {
     dataType: 'html',
     async: true,
     success: function(data) {
+      // Ensure that there is actually data there
+      if(data === null) handleBadLink(file);
+
       $("#markdown").html(converter.makeHtml(data));
+    },
+    error: function() {
+      // Handle if there is an issue with the requested page
+      handleBadLink(file);
     }
   });
 } // test()
