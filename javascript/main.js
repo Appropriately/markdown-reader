cf77fd6bd0697c655db12d7d2debd433686d8a6a
Sean Lewis <slewis@novacoast.com>
2018-11-18 21:39:42 +0000

    Dynamically populates navigation with json data

--- javascript/main.js
+++ javascript/main.js
@@ -1,8 +1,12 @@
+var HTML_DIV_LAYER = '<div class="layer mx-2"></div>';
+
 // Function that will handle the site being loaded
 function onPageLoad() {
   // Check for URL params
   var url = new URL(window.location.href);
   var page = url.searchParams.get("page");
+
+  buildNavigation();
 
   // Check if there are any URL params
   if (page === null) {
@@ -14,6 +18,53 @@ function onPageLoad() {
     loadMarkdownFromFile(page);
   } // if
 } // onPageLoad
+
+function appendFolder(json, parent) {
+  // Append layer
+  root = parent.append(HTML_DIV_LAYER).children("div:last-child");
+
+  // Add the folder name and icon
+  root.append("<span class='folder lead'><i class='fas fa-folder'></i> " +
+    json['directory'] + "</span>");
+
+  // Handle adding all the files
+  $.each(json['files'], function(index, value) {
+    to_append = "<a href='javascript:loadMarkdownFromFile(\"" +
+      value['full_path'] + "\");' role='button' class='document'>" +
+      value['file'] + "</a>";
+
+    root.append(to_append);
+  });
+
+  // Handle adding more directories
+  if ( json.hasOwnProperty('directories') ) {
+    // Recurisvely call for each one!
+    $.each(json['directories'], function(index, value) {
+      if ( json.hasOwnProperty('files') ) appendFolder(value, root);
+    });
+  } // if
+
+} // appendFolder
+
+function buildNavigation() {
+  // Retrieve file
+  $.ajax({
+    url: 'data.json',
+    type: 'get',
+    dataType: 'json',
+    async: true,
+    success: function(json) {
+      var root = $("#navigation");
+      root.html("");
+      appendFolder(json, root);
+    },
+    error: function() {
+      // Handle if there is an issue with the requested page
+      appendAlert('danger', 'There was a problem reading the folder.');
+      $("#navigation").append("<h4>Something was wrong :(</h4>");
+    }
+  });
+} // buildNavigation
 
 // Appends an alert to the page when called.
 // Some common alertTypes include success, danger, warning & info
