d2e4404a681a3717fe8e4fa027dac59d9cd63189
Sean Lewis <sean.alan.lewis@gmail.com>
2018-11-22 21:06:28 +0000

    Adds a settings menu
    The settings menu has only one option for now, toggling dark mode #2

--- javascript/theming.js
+++ javascript/theming.js
@@ -6,30 +6,50 @@
 //  --code-border-colour, --highlight-colour]
 LIGHT_THEME = ['white', 'lightgray',
   'linear-gradient(124deg, rgba(52,58,64,1) 0%, rgba(82,82,82,1) 100%)',
-  'black', '#444444', '#f8f8f8', '#dfdfdf', '#999999'];
+  'black', '#444444', '#f8f8f8', '#dfdfdf', '#999999'
+];
 DARK_THEME = ['#222222', '#333333',
   'linear-gradient(124deg, rgba(52,58,64,1) 0%, rgba(82,82,82,1) 100%)',
-  'white', 'lightgray', '#111111', 'black', '#555555'];
+  'white', 'lightgray', '#111111', 'black', '#555555'
+];
 
 
-function setDarkTheme() {
-  setTheme(DARK_THEME);
-} // setDarkTheme
+function isDarkTheme() {
+  if ($(":root").css("--main-bg-colour") === 'white') return false;
+  else return true;
+} // isDarkTheme
 
 
-function setLightTheme() {
-  setTheme(LIGHT_THEME);
-} // setLightTheme
+// Toggles between LIGHT_THEME and DARK_THEME
+function toggleDarkMode() {
+  var darkModeToggler = $("#dark-mode-toggler");
+  darkModeToggler.removeClass('fa-check-square fa-square');
+  if (isDarkTheme()) {
+    // Currently set to the dark theme
+    setTheme(LIGHT_THEME);
+    darkModeToggler.addClass("fa-square");
+  } else {
+    setTheme(DARK_THEME);
+    darkModeToggler.addClass("fa-check-square");
+  } // if
+} // toggleDarkMode
+
+
+// Ensure that the settings display the right information
+function checkSettings() {
+  // Check if in dark mode, if so set the appropriate value
+  alert('test');
+} // checkSettings
 
 
 // Given some function of the correct format, set the theme
 function setTheme(theme) {
-  document.documentElement.style.setProperty("--main-bg-colour", theme[0]);
-  document.documentElement.style.setProperty("--additional-bg-colour", theme[1]);
-  document.documentElement.style.setProperty("--navbar-bg-colour", theme[2]);
-  document.documentElement.style.setProperty("--main-text-colour", theme[3]);
-  document.documentElement.style.setProperty("--accent-colour", theme[4]);
-  document.documentElement.style.setProperty("--code-bg-colour", theme[5]);
-  document.documentElement.style.setProperty("--code-border-colour", theme[6]);
-  document.documentElement.style.setProperty("--highlight-colour", theme[7]);
+  $(":root").css("--main-bg-colour", theme[0]);
+  $(":root").css("--additional-bg-colour", theme[1]);
+  $(":root").css("--navbar-bg-colour", theme[2]);
+  $(":root").css("--main-text-colour", theme[3]);
+  $(":root").css("--accent-colour", theme[4]);
+  $(":root").css("--code-bg-colour", theme[5]);
+  $(":root").css("--code-border-colour", theme[6]);
+  $(":root").css("--highlight-colour", theme[7]);
 } // setTheme
